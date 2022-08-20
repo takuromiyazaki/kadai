302ff59496c223c869cda7ffbcd21d6d0095323e
takuromiyazaki <110997516+takuromiyazaki@users.noreply.github.com>
2022-08-20 15:22:16 +0000

    課題１

---
+++ ruby/apple.rb
@@ -0,0 +1,8 @@
+apple ="Yamanashi"
+if apple =="Aomori"
+  puts "このリンゴは青森さんです"
+  elsif apple =="Nagano"
+    puts "このリンゴは青森さんではなく、長野県産です"
+  else
+    puts "このリンゴは青森県さんでも長野県産でもありません"
+  end
---
+++ ruby/lesson5.rb
@@ -0,0 +1,8 @@
+total_price =100
+if total_price > 100
+  puts "ミカンを購入。所持金に余りあり。"
+  elsif total_price == 100
+    puts "ミカンを購入。所持金は０円。"
+  else total_price <100
+    puts "ミカンを購入することができません"
+  end
